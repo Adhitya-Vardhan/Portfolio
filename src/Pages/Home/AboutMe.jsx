b171e4ec362e6fab00451fae3cecd07f97f498b4
Adhitya <aadithyaavardhan@gmail.com>
2023-12-08 23:45:24 +0530

    first commit

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,29 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/about-me.png" alt="About Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <h1 className="skills-section--heading">Adithya Vardhan</h1>
+          <p className="hero--section-description">
+            Hello! I'm Adhitya Vardhan, a versatile developer with a passion for
+            crafting seamless digital experiences. My journey began as a PHP-SQL
+            developer, where I honed my skills in backend development. However,
+            my curiosity led me to explore the world of MERN stack development,
+            opening up new possibilities for creating robust and dynamic web
+            applications.
+          </p>
+          <p className="hero--section-description">
+            I don't just code; I design intuitive and visually appealing
+            interfaces. With hands-on experience in AWS, I've navigated the
+            cloud landscape to deploy scalable and secure applications, ensuring
+            optimal performance.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,25 @@
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey, I'm Adithya</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">Full Stack</span>{" "}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+            If you're looking for a developer who not only codes but also
+            understands the importance of a delightful user experience, let's
+            collaborate.
+          </p>
+        </div>
+        <button className="btn btn-primary">Get In Touch</button>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/hero_img.png" alt="Hero Section" />
+      </div>
+    </section>
+  );
+}
